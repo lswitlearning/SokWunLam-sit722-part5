cf6577b76749da7e56f70949c5f31ca5c067ecf0
lswitlearning <lswitlearning@gmail.com>
2024-09-22 01:21:07 +1000

    GithubAction9

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,11 +4,11 @@ variable "location" {
 }
 
 variable "container_registry_name" {
-  default     = "sit722projectpart5B"
+  default     = "sit722projectpart5C"
 }
 
 variable "kubernetes_cluster_name" {
-  default     = "sit722projectpart5B"
+  default     = "sit722projectpart5C"
 }
 
 variable app_name {
