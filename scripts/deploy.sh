64001d1b885cf7aedc4a13db02da27405eba5e15
lswitlearning <lswitlearning@gmail.com>
2024-09-22 13:39:03 +1000

    GitHubAction35

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,18 @@
-# deploy.sh
-
 #!/bin/bash
 
-# Stop the script if any error occurs
-set -euo pipefail
+# deploy.sh
 
-# Apply the Kubernetes deployment
-kubectl apply -f scripts/kubernetes/deployment.yaml
+# Stop the script if any error occurs
+set -e
+
+# Replace environment variables and create a temporary deployment file
+envsubst < scripts/kubernetes/deployment.yaml > scripts/kubernetes/deployment-withenv.yaml
+
+# Use kubectl to deploy to AKS
+kubectl apply -f scripts/kubernetes/deployment-withenv.yaml
+
+# Update the images after successful deployment
+kubectl set image deployment/book-catalog-deployment book-catalog="${CONTAINER_REGISTRY}/book_catalog:${VERSION}"
+kubectl set image deployment/inventory-management-deployment inventory-management="${CONTAINER_REGISTRY}/inventory_management:${VERSION}"
 
 echo "Microservices deployed successfully."
