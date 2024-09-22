3d4d6ed89bc31aee94eb499fc5f048a15ffa1c2d
lswitlearning <lswitlearning@gmail.com>
2024-09-22 11:58:56 +1000

    GithubAction25

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -3,8 +3,8 @@
 # Stop the script if any error occurs
 set -euo pipefail
 
-docker build -t "${CONTAINER_REGISTRY}/book_catalog:${VERSION}" -f book_catalog/Dockerfile .
+docker build -t "${CONTAINER_REGISTRY}/book_catalog:${VERSION}" -f book_catalog/Dockerfile ./book_catalog
 
-docker build -t "${CONTAINER_REGISTRY}/inventory_management:${VERSION}" -f inventory_management/Dockerfile .
+docker build -t "${CONTAINER_REGISTRY}/inventory_management:${VERSION}" -f inventory_management/Dockerfile ./inventory_management
 
 echo "Docker images built successfully."
