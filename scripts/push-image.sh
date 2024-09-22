f6ff09c42278e053147bf6cb4c5cf3a374b034be
lswitlearning <lswitlearning@gmail.com>
2024-09-22 13:33:26 +1000

    GitHubAction33

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -5,8 +5,6 @@
 # Stop the script if any error occurs
 set -euo pipefail
 
-# Log in to the container registry
-echo "${REGISTRY_PW}" | docker login "${CONTAINER_REGISTRY}" --username "${REGISTRY_UN}" --password-stdin
 
 # Push the book_catalog image
 docker push "${CONTAINER_REGISTRY}/book_catalog:${VERSION}"
@@ -15,3 +13,4 @@ docker push "${CONTAINER_REGISTRY}/book_catalog:${VERSION}"
 docker push "${CONTAINER_REGISTRY}/inventory_management:${VERSION}"
 
 echo "Docker images pushed successfully."
+
